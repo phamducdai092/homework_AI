20060000060d2dea9df30ee8bee93f8ed2adfd8a
phducdai47@gmail.com <DaiDzDellAiBang@DESKTOP-IHONG1L>
2023-09-30 10:42:27 +0700

    lab 2

---
+++ ai_lab2/src/chapter2/agent_ABCD/AgentProgram.java
@@ -0,0 +1,18 @@
+package chapter2.agent_ABCD;
+
+import chapter2.agent_ABCD.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		if(p.getLocationState()==LocationState.DIRTY) {
+			return new DynamicAction("SUCK");
+		}else if(p.getAgentLocation().equals("A")) {
+			return new DynamicAction("RIGHT");
+		}else if(p.getAgentLocation().equals("B")) {
+			return new DynamicAction("LEFT");
+		}
+		return NoOpAction.NO_OP;
+		
+	}
+}
